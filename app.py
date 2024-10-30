129be85562f0007fd762cb306e066eb834215725
Emythiel <18901520+Emythiel@users.noreply.github.com>
2024-10-30 08:55:53 +0100

    add routes for reviews and occupancy

--- app.py
+++ app.py
@@ -4,14 +4,13 @@ import requests, app_csv_convert
 
 app = Flask(__name__)
 
-# Routes for Rooms Service
+# Room service routes
 @app.route('/rooms')
 def get_rooms():
     room_url = 'http://ka-rooms:5000/rooms'
     response = requests.get(room_url)
     return jsonify(response.json())
 
-# Routes for rooms data as csv
 @app.route('/rooms/csv')
 def get_rooms_csv():
     room_url = 'http://ka-rooms:5000/rooms'
@@ -19,14 +18,41 @@ def get_rooms_csv():
     json_data = response.json()
     return app_csv_convert.convert_json_to_csv(json_data, filename="rooms.csv")
 
-# Bar Sales route
+# Reviews routes
+@app.route('/reviews')
+def get_reviews():
+    review_url = 'http://ka-reviews:5000/reviews'
+    response = requests.get(review_url)
+    return jsonify(response.json())
+
+@app.route('/reviews/csv')
+def get_reviews_csv():
+    review_url = 'http://ka-reviews:5000/reviews'
+    response = requests.get(review_url)
+    json_data = response.json()
+    return app_csv_convert.convert_json_to_csv(json_data, filename="reviews.csv")
+
+# Occupancy routes
+@app.route('/occupancy')
+def get_occupancy():
+    occupancy_url = 'http://ka-occupancy:5000/occupancy'
+    response = requests.get(occupancy_url)
+    return jsonify(response.json())
+
+@app.route('/occupancy/csv')
+def get_occupancy_csv():
+    occupancy_url = 'http://ka-occupancy:5000/occupancy'
+    response = requests.get(occupancy_url)
+    json_data = response.json()
+    return app_csv_convert.convert_json_to_csv(json_data, filename="occupancy.csv")
+
+# Bar sales routes
 @app.route('/barsales')
 def get_bar_sales():
     bar_sales_url = 'http://ka-bar-sales:5000/barsales'
     response = requests.get(bar_sales_url)
     return jsonify(response.json())
 
-# Bar sales data as csv
 @app.route('/barsales/csv')
 def get_bar_sales_csv():
     bar_sales_url = 'http://ka-bar-sales:5000/barsales'
