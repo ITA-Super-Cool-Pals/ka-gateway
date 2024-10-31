3d894e9f820cbeda43bc02fe8a672aff7735937f
Emythiel <18901520+Emythiel@users.noreply.github.com>
2024-10-31 14:02:11 +0100

    add bookings and guests to gateway

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import requests, app_csv_convert
 
 app = Flask(__name__)
 
+
 # Room service routes
 @app.route('/rooms')
 def get_rooms():
@@ -17,6 +18,7 @@ def get_rooms_csv():
     response = requests.get(room_url)
     json_data = response.json()
     return app_csv_convert.convert_json_to_csv(json_data, filename="rooms.csv")
+
 
 # Reviews routes
 @app.route('/reviews')
@@ -52,6 +54,76 @@ def get_reviews_csv():
     json_data = response.json()
     return app_csv_convert.convert_json_to_csv(json_data, filename="reviews.csv")
 
+# Booking routes
+@app.route('/bookings')
+def get_bookings():
+    bookings_url = 'http://ka-bookings:5000/bookings'
+    response = requests.get(bookings_url)
+    return jsonify(response.json())
+
+@app.route('/bookings/<int:id>')
+def get_bookings_single(id):
+    bookings_url = f'http://ka-bookings:5000/bookings/{id}'
+    response = requests.get(bookings_url)
+    return jsonify(response.json())
+
+@app.route('/bookings', methods=['POST'])
+def create_booking():
+    bookings_url = 'http://ka-bookings:5000/create'
+    bookings_data = request.get_json()
+
+    # Send POST request to ka-bookings service
+    bookings_response = requests.post(url=bookings_url, json=bookings_data)
+
+    # Check status code of response from ka-bookings
+    if bookings_response.status_code == 201:
+        return jsonify({"message:" "booking created"}), 201
+    else:
+        return jsonify({"error": "server error"}), bookings_response.status_code
+
+@app.route('/bookings/csv')
+def get_bookings_csv():
+    bookings_url = 'http://ka-bookings:5000/bookings'
+    response = requests.get(bookings_url)
+    json_data = response.json()
+    return app_csv_convert.convert_json_to_csv(json_data, filename="bookings.csv")
+
+
+# Guest routes
+@app.route('/guests')
+def get_guests():
+    guests_url = 'http://ka-guests:5000/guests'
+    response = requests.get(guests_url)
+    return jsonify(response.json())
+
+@app.route('/guests/<int:id>')
+def get_guests_single(id):
+    guests_url = f'http://ka-guests:5000/guests/{id}'
+    response = requests.get(guests_url)
+    return jsonify(response.json())
+
+@app.route('/guests', methods=['POST'])
+def create_guest():
+    guests_url = 'http://ka-guests:5000/guests'
+    guests_data = request.get_json()
+
+    # Send POST request to ka-guests service
+    guests_response = requests.post(url=guests_url, json=guests_data)
+
+    # Check status code of response from ka-guests
+    if guests_response.status_code == 201:
+        return jsonify({"message": "guest created"}), 201
+    else:
+        return jsonify({"error": "server error"}), guests_response.status_code
+
+@app.route('/guests/csv')
+def get_guests_csv():
+    guests_url = 'http://ka-guests:5000/guests'
+    response = requests.get(guests_url)
+    json_data = response.json()
+    return app_csv_convert.convert_json_to_csv(json_data, filename="guests.csv")
+
+
 # Occupancy routes
 @app.route('/occupancy')
 def get_occupancy():
@@ -65,6 +137,7 @@ def get_occupancy_csv():
     response = requests.get(occupancy_url)
     json_data = response.json()
     return app_csv_convert.convert_json_to_csv(json_data, filename="occupancy.csv")
+
 
 # Bar sales routes
 @app.route('/barsales')
